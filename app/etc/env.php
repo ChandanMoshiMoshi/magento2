b778ca83d3409685ba74a1d3aaf20d48eb691c04
ChandanMoshiMoshi <58543053+ChandanMoshiMoshi@users.noreply.github.com>
2019-12-06 15:18:01 +0530

    add env file

---
+++ app/etc/env.php
@@ -0,0 +1,67 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => '27fec33ff606ad47f873d23561eed9b2'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'sacred_new',
+                'username' => 'root',
+                'password' => 'root',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '373_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '373_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'cache_types' => [
+        'config' => 0,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 0,
+        'reflection' => 0,
+        'db_ddl' => 0,
+        'compiled_config' => 0,
+        'eav' => 0,
+        'customer_notification' => 0,
+        'config_integration' => 0,
+        'config_integration_api' => 0,
+        'full_page' => 0,
+        'config_webservice' => 0,
+        'translate' => 0,
+        'vertex' => 0
+    ],
+    'install' => [
+        'date' => 'Tue, 05 Nov 2019 07:37:07 +0000'
+    ]
+];
